8e16a92fc1d68d9f87a2e883a63d782de6275d44
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-28 18:55:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var animal = 'dog'
+var yourAnimal = 'cat'
 
 function myAnimal() {
   return animal
@@ -10,7 +11,7 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
 
   
-  return animal
+  return yourAnimal
 }
 
 function add2(n) {
